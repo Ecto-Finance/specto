df9807b22cfe13f493326b64d9d13a44130d7421
Brian Cooper <20056195+coopbri@users.noreply.github.com>
2022-03-19 03:03:48 -0500

    add Apollo config

---
+++ src/lib/apollo/client.ts
@@ -0,0 +1,38 @@
+import { useMemo } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+  from,
+} from "@apollo/client";
+
+import { errorLink, httpLink } from "lib/apollo/links";
+
+let apolloClient: ApolloClient<NormalizedCacheObject>;
+
+/**
+ * Create Apollo client.
+ */
+const createApolloClient = () =>
+  new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: from([errorLink, httpLink]),
+    cache: new InMemoryCache(),
+  });
+
+export const initializeApollo = (initialState = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  if (initialState) {
+    _apolloClient.cache.restore(initialState!);
+  }
+
+  if (typeof window === "undefined") return _apolloClient;
+
+  apolloClient = apolloClient ?? _apolloClient;
+
+  return apolloClient;
+};
+
+export const useApollo = (initialState: any) =>
+  useMemo(() => initializeApollo(initialState), [initialState]);
--- src/lib/config/env.ts
+++ src/lib/config/env.ts
@@ -6,5 +6,10 @@
  */
 
 export const NODE_ENV = process.env.NODE_ENV;
+
+// base GraphQL API URL
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+// web3
 export const ETHERSCAN_API_KEY = process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY;
 export const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID;
