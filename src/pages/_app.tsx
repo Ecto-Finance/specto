d49dc6432ca4eb17c63a4edbc6f65294832a811c
Brian Cooper <20056195+coopbri@users.noreply.github.com>
2022-03-19 08:10:01 +0000

    Apollo GraphQL Integration (#3)
    * deps: add @apollo/client, graphql
    
    * add Apollo config
    
    * add GraphQL code generator + config, update dev script
    
    * add base env and production env files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
 import { WagmiProvider } from "wagmi";
 
 import { baseWagmiProvider } from "lib/wagmi";
+import { useApollo } from "lib/apollo/client";
 
 /**
  * Custom app entry point.
  */
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   return (
-    <WagmiProvider provider={baseWagmiProvider}>
-      <Component {...pageProps} />
-    </WagmiProvider>
+    <ApolloProvider client={apolloClient}>
+      <WagmiProvider provider={baseWagmiProvider}>
+        <Component {...pageProps} />
+      </WagmiProvider>
+    </ApolloProvider>
   );
 };
 
