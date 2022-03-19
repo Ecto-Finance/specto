e91163c8bc5daa5ad2bfb18d79186056148b7172
Brian Cooper <20056195+coopbri@users.noreply.github.com>
2022-03-19 02:53:22 -0500

    add eth-sdk config

---
+++ src/lib/config/contracts.ts
@@ -0,0 +1,17 @@
+import type { EthSdkConfig } from "@dethcrypto/eth-sdk";
+
+/**
+ * Per-network smart contract configuration.
+ */
+const contracts: EthSdkConfig["contracts"] = {
+  mainnet: {
+    // TODO
+    // specto: "0x...",
+  },
+  rinkeby: {
+    // TODO
+    // specto: "0x...",
+  },
+};
+
+export default contracts;
